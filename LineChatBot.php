b6104d5b8de8ac250084c1fa077fe96eaa104380
[kogger023@gmail.com] <[SupanunJ]>
2017-10-29 18:50:22 +0700

    button

--- LineChatBot.php
+++ LineChatBot.php
@@ -44,7 +44,7 @@ if (!is_null($events['events']))
 			else if ($getText=='ดูข้อมูลร้านค้า')
 			{
         pushMessage($userID,textBuild('ร้านขนมข้าวตังเสวยแม่ณี  สามารถติดต่อทางร้านได้ที่เบอร์  0818178962 ทางร้านขอขอบพระคุณลูกค้าทุกท่านที่ใช้บริการ'),$access_token);
-        pushMessage($userID,locationBuild(),$access_token);
+        pushMessage($userID,buttonBuild(),$access_token);
 			}
 			else if ($getText=='ดูข้อมูลส่วนตัว')
 			{
@@ -97,6 +97,26 @@ function locationBuild()
       'address' => 'บ้านบัวทอง2',
       'latitude' => '13.894987',
       'longitude' => '100.402295'
+  ];
+  return $messages;
+}
+
+function buttonBuild()
+{
+  $messages = [
+      'type' => 'template',
+      'altText' => 'this is a buttons template',
+      'template' => [
+          'type' => 'buttons',
+          'thumbnailImageUrl' => 'https://example.com/bot/images/image.jpg',
+          'title' => 'Menu',
+          'text' => 'Please select',
+          'actions' => [
+                'type' => 'message',
+                'label' => 'eiei',
+                'text' => 'eiei'
+            ]
+        ]
   ];
   return $messages;
 }
