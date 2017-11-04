9f9edc962c18938b504c3f261caa9c1ee082b0c4
[kogger023@gmail.com] <[SupanunJ]>
2017-11-05 00:06:28 +0700

    gshv

--- LineChatBot.php
+++ LineChatBot.php
@@ -27,7 +27,7 @@ if (!is_null($events['events']))
 
 			if($getText=='qqq')
 			{
-				$result = $connention->query("SELECT line_id FROM Customer");
+				$result = $connention->query("SELECT * FROM Customer");
 				if($result!=null)
 				{
 					pushMessage($userID,textBuild('yes'),$access_token);
