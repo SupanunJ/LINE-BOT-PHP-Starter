1f707246780ac45004dccf972d7f8a0632c57cfc
[kogger023@gmail.com] <[SupanunJ]>
2017-11-08 14:08:55 +0700

    query supanun

--- LineChatBot.php
+++ LineChatBot.php
@@ -27,12 +27,12 @@ if (!is_null($events['events']))
 
 			if($getText=='ยังไง')
 			{
-				$result = $connention->query("SELECT * FROM customer");
+				$result = $connention->query("SELECT * FROM customer WHERE line_id = 'Supanun'");
 				if($result!=null)
 				{
 					pushMessage($userID,textBuild('มันไม่เป็นNULLเว้ย'),$access_token);
 					pushMessage($userID,textBuild($result->rowCount()),$access_token);
-					pushMessage($userID,textBuild($result),$access_token);
+					pushMessage($userID,textBuild($result.line_id),$access_token);
 				}
 			}
 
